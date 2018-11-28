dfeed78cc8be901f50e32753bedc3f77e263bcb1
elenays <helenasp@yandex.ru>
2018-11-28 13:05:50 +0300

    Данные о товаре берутся из json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let request = require('request-promise');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Store' });
+router.get('/goods/vanna', function(req, res, next) {
+    request('https://api.myjson.com/bins/1blpju')
+        .then(function (item) {
+            item = JSON.parse(item);
+            res.render('index', { title: item.title, article: item.article, size: item.size, color: item.color,
+                description: item.description, price: item.price, img: item.img });
+        })
+        .catch(function (err) {
+            // Crawling failed...
+        });
 });
+
+
 
 module.exports = router;
