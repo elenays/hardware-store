5e66844866bd6ac90ca2f81b113cb7213f8c4ca7
elenays <helenasp@yandex.ru>
2018-12-03 22:04:16 +0300

    Данные берутся из Mongodb

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
+const db = require('./config/db');
+let ObjectID = require('mongodb').ObjectID;
+const dbName = 'firstproject';
+let db_cursor = null;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,14 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -36,6 +33,32 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err);
+
+    db_cursor =  database.db(dbName);
+
+});
+
+app.get('/goods/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {'_id': new ObjectID(id)};
+    db_cursor.collection('goods').findOne(details)
+        .then(function (item) {
+            res.render('index', {
+                title: item.title, article: item.article, size: item.size, color: item.color,
+                description: item.description, price: item.price, img: item.img});
+        })
+        .catch(function (err) {
+            if(err) throw err;
+        });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 module.exports = app;
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url : "mongodb://customer:customercustomer1@ds257241.mlab.com:57241/firstproject"
+
+};
